8e4955c8b1e31462588f8250da3d9432798758ae
saikumar0204 <saichodipilli@gmail.com>
2021-07-11 20:46:54 +0530

    Some update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app= express()
 
-const port= process.env.PORT||3000;
+const port= process.env.PORT||80;
 // const ExpressError = require('utils/ExpressError');
 const cors =require('cors')
 
